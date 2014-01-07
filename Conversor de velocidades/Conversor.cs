56e50d28a2cf559cde731fe9ae8b84f66df287a9
AlexJPZ17 <alexjpz17@gmail.com>
2014-01-07 11:39:35 -0600

    C Sharp Conversor de velocidades

---
+++ Conversor de velocidades/Conversor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversor_de_velocidades
+{
+    class Conversor
+    {
+        //Declaramos una variable
+        double velocidad;
+        //Método que obtiene el dato m/s y lo convierte a km/h
+        public void Conversor1()
+        {
+            Console.WriteLine("Introduzca una velocidad en m/s");
+            velocidad = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(velocidad + " m/s = " + velocidad * 3600 / 1000 + " km/h");
+        }
+        //Método que obtiene el dato km/h y lo convierte a m/s
+        public void Conversor2()
+        {
+            Console.WriteLine("Introduzca una velocidad en km/h");
+            velocidad = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(velocidad + " km/h = " + velocidad * 1000 / 3600 + " m/s");
+        }
+    }
+}
---
+++ Conversor de velocidades/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversor_de_velocidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaramos dos variable
+            int opcion, repetir = 0;
+            //Mandamos a llamar a la clase Conversor
+            Conversor con = new Conversor();
+            //Instrucción do while que se repetirá de acuerdo a lo que este en la variable repetir
+            do
+            {
+                //Mostramos los mensajes y obtenemos el dato ingresado
+                Console.WriteLine("Escoja una de las opciones");
+                Console.WriteLine("1 - Convertir m/s a km/h");
+                Console.WriteLine("2 - Convertir km/h a m/s");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                //Instrucción switch que evalúa la variable opción
+                switch (opcion)
+                {
+                    case 1:
+                        con.Conversor1();
+                        break;
+                    case 2:
+                        con.Conversor2();
+                        break;
+                    default:
+                        Console.WriteLine("Opción errónea");
+                        break;
+                }
+                //Colocamos un mensaje con una opción 
+                Console.WriteLine("Si desea repetir coloque el valor de 1 o cualquier tecla para salir");
+                repetir = Convert.ToInt32(Console.ReadLine());
+            } while (repetir == 1);
+        }
+    }
+}
